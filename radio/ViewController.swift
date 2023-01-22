d28ea2ea0367d1f09d1810d5f7a532903b8c03a0
Guzei <igor@guzei.com>
2023-01-22 17:51:29 +0300

    v. 0.1. minimum code for listen one radio station. Without controls.

--- radio/ViewController.swift
+++ radio/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+//import AVFAudio
+//import Foundation
+import MediaPlayer          // AVPlayer()
+//import AVKit
+//import Kingfisher
+
 
 class ViewController: UIViewController {
 
+    let bfm = URL(string: "https://bfm.hostingradio.ru:8004/fm32")
+    var player = AVPlayer()
+
+//    private lazy var player: AVAudioPlayer = {
+//        do {
+//            let player = try AVAudioPlayer(contentsOf: bfm!)
+//            return player
+//        }
+//        catch {
+//            print("Error")
+//        }
+//        return player
+//    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+
+        let playerItem = AVPlayerItem(url: bfm!)
+        player = AVPlayer(playerItem: playerItem)
+        player.play()
     }
-
-
 }
-
