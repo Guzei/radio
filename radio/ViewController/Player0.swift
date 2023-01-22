dba2cd6457b944114f4c843b984f1442e32446a3
Guzei <igor@guzei.com>
2023-01-22 23:54:52 +0300

    minimum controls

--- radio/ViewController/Player0.swift
+++ radio/ViewController/Player0.swift
@@ -15,44 +15,62 @@ import MediaPlayer          // AVPlayer()
 
 class Player0: UIViewController {
 
-    let bfm  = URL(string: "https://bfm.hostingradio.ru:8004/fm32")
-    let alfa = URL(string: "https://radioalfa1111.blogspot.com")
-    let folkObondoru = URL(string: "https://player.europa.kg:1105/stream")
-
+    // An object that provides the interface to control the player’s transport behavior.
+    // https://developer.apple.com/documentation/avfoundation/avplayer
     var player = AVPlayer()
 
-    private lazy var pauseButton: UIButton = {
-        $0.setTitle("Pause", for: .normal)
-        $0.backgroundColor = .systemBlue
-        $0.layer.cornerRadius = 4
-        $0.layer.shadowOffset = CGSize(width: 4, height: 4)
-        $0.layer.shadowRadius = 4
-        $0.layer.shadowColor = UIColor.black.cgColor
-        $0.layer.shadowOpacity = 0.7
+    private lazy var svController: UIStackView = {
+        $0.axis = .horizontal
+        $0.spacing = 16
         $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        $0.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        return $0
+    }(UIStackView())
+
+    private lazy var playButton: UIButton = {
+        $0.setImage(UIImage(systemName: "play.circle"), for: .normal)
+        $0.addTarget(self, action: #selector(pressPlayButton), for: .touchUpInside)
         return $0
     }(UIButton())
+    @objc func pressPlayButton() {
+        player.play()
+    }
 
-    @objc func pressButton() {
+    private lazy var pauseButton: UIButton = {
+        $0.setImage(UIImage(systemName: "pause.circle"), for: .normal)
+        $0.addTarget(self, action: #selector(pressPauseButton), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    @objc func pressPauseButton() {
         player.pause()
     }
 
+    private lazy var stopButton: UIButton = {
+        $0.setImage(UIImage(systemName: "stop.circle"), for: .normal)
+        $0.addTarget(self, action: #selector(pressStopButton), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    @objc func pressStopButton() {
+        player.replaceCurrentItem(with: nil)
+    }
+
+    let bfm  = URL(string: "https://bfm.hostingradio.ru:8004/fm32")
+    let alfa = URL(string: "https://radioalfa1111.blogspot.com")
+    let folkObondoru = URL(string: "https://player.europa.kg:1105/stream")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        view.addSubview(pauseButton)
-
+        svController.addArrangedSubview(playButton)
+        svController.addArrangedSubview(pauseButton)
+        svController.addArrangedSubview(stopButton)
+        view.addSubview(svController)
 
         NSLayoutConstraint.activate([
-            pauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pauseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            svController.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            svController.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
 
         player = AVPlayer(playerItem: AVPlayerItem(url: folkObondoru!))
-        player.play()
     }
 }
